43652784763152f1a2e27e70024608280dc95311
Severag <odonobii@gmail.com>
2024-11-30 23:44:49 -0600

    Initial commit

---
+++ Day 16/day_16.py
@@ -0,0 +1,56 @@
+import itertools, re
+import numpy as np
+
+
+def read_file(filename):
+    with open( filename, 'r') as f:
+        data = [int(line.strip()) for line in f]
+    
+    return data
+
+
+
+def solve(data, do_1=True, do_2=True):
+    p1 = part1(data) if do_1 else None
+    p2 = part2(data) if do_2 else None
+    
+    return p1, p2
+
+
+
+def part1(data):
+    return
+
+
+
+def part2(data):
+    return
+
+
+
+def check(myanswer, answer):
+    if answer:  # compare each pre-known answer with its matching part's answer
+        for my_ans, ans in zip(myanswer, answer):
+            if ans is not None:
+                if np.array_equal(my_ans, ans):
+                    print(f"\tCheck passed!   \t{my_ans}")
+                else:
+                    print(f"\n\t{'ERROR':{'*'}^50}\n" + 
+                          f"\tCorrect answer: \t{ans}\n"
+                          f"\tReturned answer:\t{my_ans}")
+    else:  # don't have a pre-known answer
+        print(f"\t{myanswer}")
+
+
+
+puzzles = [['test_case.txt',    [None, None]],
+           ['puzzle_input.txt', []]]
+
+for problem in puzzles:
+    filename, answers = problem
+    data = read_file(filename)
+    
+    do_parts = [ans is not None for ans in answers] if answers else [True, True]    
+    print(f"\n\n{filename}")
+    results = solve(data, *do_parts)
+    check(results, answers)
