aa7232d32292661d99935cdde32a3181aafc669d
Severag <odonobii@gmail.com>
2024-12-07 01:27:39 -0600

    Finished day 6

--- Day 6/day_6.py
+++ Day 6/day_6.py
@@ -63,8 +63,7 @@ def part2(data):
     dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # directions
     facing = 0
     have_ended = False
-    can_cycle = False
-    extra_blocks = []
+    potentials = []
     
     while not have_ended:
         # move to next roadblock
@@ -75,13 +74,51 @@ def part2(data):
             board[new_loc[0], new_loc[1]] = -1  # mark where we've been
             paths[new_loc[0], new_loc[1], facing] = 1
             
-            # for checking if we can create a cycle here
-            turn_loc = new_loc + dirs[next_facing]
-            # if we turn right here, have we been this way before?
-            # can_cycle = paths[turn_loc[0], turn_loc[1], next_facing] == 1
-            can_cycle = circles_back(board, paths, turn_loc, next_facing)
-            # we'll do something with this in the next iteration, after checking
-            # for out-of-boundness and blockage-ness
+            # advance
+            new_loc += dirs[facing]
+            
+            # if we're going out of bounds
+            if np.any([new_loc >= board.shape, new_loc < 0]):
+                have_ended = True
+                break
+            
+            # if we haven't been this way before, check if placing a block here
+            # would create a cycle
+            if board[new_loc[0], new_loc[1]] != -1:
+                temp_board = board.copy()
+                temp_paths = paths.copy()
+                
+                # create new block
+                temp_board[new_loc[0], new_loc[1]] = 1  # add block here
+                
+                # reset to just before hitting it
+                temp_loc = tuple(new_loc - dirs[facing])
+                
+                if circles_back(temp_loc, temp_board, temp_paths, facing):
+                    potentials.append(temp_loc)
+        
+        # take one step back to not be directly on top of the block
+        new_loc = tuple(new_loc - dirs[facing])
+        
+        loc = new_loc
+        facing = next_facing
+    
+    return len(potentials)
+
+
+
+def circles_back(loc, board, paths, facing):    
+    dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # directions
+    have_ended = False
+    
+    while not have_ended:
+        # move to next roadblock
+        new_loc = np.array(loc)
+        next_facing = (facing + 1) % len(dirs)  # turn 90 degrees right
+        
+        while board[new_loc[0], new_loc[1]] < 1:
+            board[new_loc[0], new_loc[1]] = -1  # mark where we've been
+            paths[new_loc[0], new_loc[1], facing] = 1
             
             # advance
             new_loc += dirs[facing]
@@ -91,9 +128,9 @@ def part2(data):
                 have_ended = True
                 break
             
-            if can_cycle:
-                extra_blocks.append(tuple(new_loc))
-                can_cycle = False
+            # if we've been this way before
+            if board[new_loc[0], new_loc[1]] == -1 and paths[new_loc[0], new_loc[1], facing] == 1:
+                return True
         
         # take one step back to not be directly on top of the block
         new_loc = tuple(new_loc - dirs[facing])
@@ -101,38 +138,8 @@ def part2(data):
         loc = new_loc
         facing = next_facing
     
-    return len(extra_blocks)
-
-
-
-def circles_back(board, path, loc, facing):
-    if facing == 0:  # north
-        block = np.where(board[:loc[0], loc[1]] > 0)
-        if len(block[0]) > 0:
-            top = block[0][-1]
-            if np.any(path[top:loc[0] + 1, loc[1], facing] == 1):
-                return True
-    elif facing == 1:  # east
-        block = np.where(board[loc[0], loc[1]:] > 0)
-        if len(block[0]) > 0:
-            end = block[0][0] + loc[1]
-            if np.any(path[loc[0], loc[1]:end, facing] == 1):
-                return True
-    elif facing == 2:  # south
-        block = np.where(board[loc[0]:, loc[1]] > 0)
-        if len(block[0]) > 0:
-            bottom = block[0][0] + loc[0]
-            if np.any(path[loc[0]:bottom, loc[1], facing] == 1):
-                return True
-    elif facing == 3:  # west
-        block = np.where(board[loc[0], :loc[1] + 1] > 0)
-        if len(block[0]) > 0:
-            start = block[-1][0]
-            if np.any(path[loc[0], start:loc[1], facing] == 1):
-                return True
     return False
     
-
 
 
 def check(myanswer, answer):
@@ -150,12 +157,10 @@ def check(myanswer, answer):
 
 
 
-puzzles = [#['test_case.txt',    [41, 6]],
-           #['test_case_2.txt',  [None, 1]],
+puzzles = [['test_case.txt',    [41, 6]],
+           ['test_case_2.txt',  [None, 1]],
            ['test_case_3.txt',  [None, 3]],
-           ['puzzle_input.txt', []]]  # 5014 is too high
-                                      # 484 is too low
-                                      # 485 is too low
+           ['puzzle_input.txt', []]]  
 
 for problem in puzzles:
     filename, answers = problem
