8c155d879689b275433dd2c3a9a84a4d57152bdc
Severag <odonobii@gmail.com>
2024-12-11 00:34:38 -0600

    Day 11 done

--- Day 11/day_11.py
+++ Day 11/day_11.py
@@ -1,12 +1,13 @@
-import itertools, re
 import numpy as np
+from collections import defaultdict
+from functools import lru_cache
 
 
 def read_file(filename):
     with open( filename, 'r') as f:
-        data = [int(line.strip()) for line in f]
+        data = [line.strip().split(' ') for line in f]
     
-    return data
+    return data[0]
 
 
 
@@ -19,12 +20,69 @@ def solve(data, do_1=True, do_2=True):
 
 
 def part1(data):
-    return
+    line = [int(val) for val in data]
+    
+    for blink in range(25):
+        idx = 0
+        while idx < len(line):
+            val = line[idx]
+            
+            if val == 0:
+                line[idx] = 1
+            else:
+                val_str = str(val)
+                digits = len(val_str)
+                
+                if digits % 2 == 0:
+                    val1 = int(val_str[:digits // 2])
+                    val2 = int(val_str[digits // 2:])
+                    
+                    line[idx] = val1
+                    line.insert(idx + 1, val2)
+                    
+                    idx += 1  # skip inserted value
+                else:
+                    line[idx] *= 2024
+            
+            idx += 1
+    
+    return len(line)
 
 
 
-def part2(data):
-    return
+def part2(data, cycles=75):
+    
+    num_dict = defaultdict(int)
+    
+    for val in data:
+        num_dict[val] += 1
+    
+    for blink in range(cycles):
+        next_dict = defaultdict(int)
+        
+        for key,val in num_dict.items():
+            for new_key in get_evolution(key):
+                next_dict[new_key] += val
+        
+        num_dict = next_dict
+    
+    total = 0
+    for key,val in num_dict.items():
+        total += val
+    
+    return total
+
+
+
+@lru_cache
+def get_evolution(val_str):
+    if val_str == '0':
+        return ['1',]
+    if len(val_str) % 2 == 0:
+        half_digits = len(val_str) // 2
+        return [str(int(val)) for val in [val_str[:half_digits], val_str[half_digits:]]]
+    else:
+        return [str(int(val_str) * 2024),]
 
 
 
@@ -43,7 +101,7 @@ def check(myanswer, answer):
 
 
 
-puzzles = [['test_case.txt',    [None, None]],
+puzzles = [['test_case.txt',    [55312, None]],
            ['puzzle_input.txt', []]]
 
 for problem in puzzles:
