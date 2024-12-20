848b54faf4c580449791d37f19e79d7cf1f15def
Severag <odonobii@gmail.com>
2024-12-20 17:42:48 -0600

    Solved Day 20

--- Day 20/day_20.py
+++ Day 20/day_20.py
@@ -1,12 +1,12 @@
-import itertools, re
+import heapq
 import numpy as np
 
 
 def read_file(filename):
     with open( filename, 'r') as f:
-        data = [int(line.strip()) for line in f]
+        data = [list(line.strip()) for line in f]
     
-    return data
+    return np.array(data)
 
 
 
@@ -18,13 +18,72 @@ def solve(data, do_1=True, do_2=True):
 
 
 
-def part1(data):
-    return
+def part1(data, cheat_dist=2):
+    dist, path = dijkstra(data)
+    
+    path_arr = np.array(path)
+    dists_3D = np.abs(path_arr - path_arr[:, np.newaxis, :])
+    dists = np.sum(dists_3D, axis=2)  # manhattan distance
+    
+    path_idx = np.arange(0, len(path_arr))
+    path_dists  = np.abs(path_idx - path_idx[:, np.newaxis])
+    
+    if data.size < 300:  # test case
+        shortcuts = (dists <= cheat_dist) & (path_dists > cheat_dist)
+        
+        cheats = dict()
+        for p1, p2 in zip(*np.where(shortcuts)):
+            loc = (p1, p2) if p1 < p2 else (p2, p1)
+            time_saved = path_dists[loc] - dists[loc]
+            cheats[loc] = time_saved
+        
+        top_cheats = sorted(list(cheats.values()), reverse=True)
+        
+        return top_cheats[:5]
+    else:
+        time_saved = path_dists - dists
+        shortcuts = (dists <= cheat_dist) & (time_saved >= 100)
+        return np.count_nonzero(shortcuts) // 2
+
+
+
+deltas = [[-1, 0],
+          [ 0, 1],
+          [ 1, 0],
+          [ 0,-1]]
+def dijkstra(board):
+    start = tuple(np.squeeze(np.where(board == 'S')))
+    end = tuple(np.squeeze(np.where(board == 'E')))
+    
+    open_list = [(0, start, [start])]
+    closed_set = set()
+    
+    while open_list:
+        dist, loc, path = heapq.heappop(open_list)
+        
+        # end condition
+        if loc == end:
+            return dist, path
+        
+        # check for repeat
+        if loc in closed_set:
+            raise ValueError
+        closed_set.add(loc)
+        
+        # advance
+        for dr, dc in deltas:
+            cand = (loc[0] + dr, loc[1] + dc)
+            
+            if cand not in closed_set and board[cand] != '#':
+                new_state = (dist + 1, cand, path + [cand,])
+                heapq.heappush(open_list, new_state)
+    
+    return -1
 
 
 
 def part2(data):
-    return
+    return part1(data, 20)
 
 
 
@@ -43,7 +102,7 @@ def check(myanswer, answer):
 
 
 
-puzzles = [['test_case.txt',    [None, None]],
+puzzles = [['test_case.txt',    [[64, 40, 38, 36, 20], [76, 76, 76, 74, 74]]],
            ['puzzle_input.txt', []]]
 
 for problem in puzzles:
